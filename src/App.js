f6fce398279dc00bdfe6e4639f081a81be0fe47d
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-07-31 10:52:27 +0530

    add basic setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import MainPage from './Main/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Close.io</h1>      
+      <h1>Close.io</h1>
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<MainPage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
