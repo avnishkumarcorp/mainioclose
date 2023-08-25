cf1501715da712231deb401918590c95eb61e506
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-08-25 16:38:47 +0530

    add sign up funcionality

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,10 +1,51 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 import "./Login.scss"
 import { image1 } from "../Images/imageFile"
 import corpseedlogo from "../Images/corpseed-logo.png"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
 
 const Login = () => {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  })
+
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const userEmailRef = useRef()
+  const userPasswordRef = useRef()
+
+  const UserInfo = (e) => {
+    setUserData((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+
+  const userLogin = (e) =>{
+    e.preventDefault()
+    setLoading(true);
+    if (userEmailRef.current.value === "") {
+      userEmailRef.current.style.border = "1px solid red"
+      setError(true);
+      return
+    }
+    if (userPasswordRef.current.value === "") {
+      userPasswordRef.current.style.border = "1px solid red"
+      setError(true);
+      return
+    }
+    navigate("/")
+
+    const userLogin = async  () =>{
+      const loginData = await axios.post("")
+    }
+
+
+  }
+
+
+
   return (
     <div className="login-page">
       <div className="login-form">
@@ -18,6 +59,9 @@ const Login = () => {
                 className="input-design"
                 type="email"
                 placeholder="Enter Your Email"
+                name="email"
+                ref={userEmailRef}
+                onChange={(e) => UserInfo(e)}
                 required
               />
             </div>
@@ -27,11 +71,15 @@ const Login = () => {
                 className="input-design"
                 type="password"
                 placeholder="Enter Your password"
+                name="password"
+                ref={userPasswordRef}
+                onChange={(e) => UserInfo(e)}
                 required
               />
             </div>
+            {error ? <div><span className="text-danger">Email or Password can't be Blank</span></div> : " "}
             <div className="center-btn">
-              <button className="login-button">Login</button>
+              <button onClick={(e) => userLogin(e)}  className="login-button">  {loading ?  "loading" :  "Login"   }</button>
             </div>
             <p className="dont-account">
               Don't have an Account{" "}
