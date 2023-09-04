9b5734cf9a8d889468bcf66a0ab9ed40d69ab194
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-09-04 10:55:13 +0530

    add home page route and solve  unique child error in inbox  table

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<div>Home page</div>} />
           <Route path="/counter" element={<CounterExample />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
