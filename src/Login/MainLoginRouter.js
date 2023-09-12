468cb8cd996dd07ead5fc83ecea50201a36cb3c8
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-09-12 16:12:54 +0530

    add logo in header and also change the forget password timer

--- src/Login/MainLoginRouter.js
+++ src/Login/MainLoginRouter.js
@@ -1,12 +1,20 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
 import "./Login.scss"
+import logoImage from "../Images/main-logo.png";
+import "./MainLoginRoute.scss"
 
 const MainLoginRouter = () => {
   return (
     <div>
       <div className="container logo-header">
-        <h1 className="logo-icon">Logo</h1>
+        {/* <h1 className="logo-icon">Logo</h1> */}
+        <div className="logo">
+          <div className="erp-image">
+            <img className="main-logo-image" src={logoImage}  />
+          </div>
+        <h2 className="erp-text">ERP</h2>
+        </div>
       </div>
       <Outlet />
     </div>
