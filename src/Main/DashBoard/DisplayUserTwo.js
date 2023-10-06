4f1a831bf13111d8765c09b6498ce579c523482a
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-10-06 14:41:06 +0530

    add render cell function and also add html element and add function or how to get data

--- src/Main/DashBoard/DisplayUserTwo.js
+++ src/Main/DashBoard/DisplayUserTwo.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import React, { useEffect } from "react"
 import { useState } from "react"
 import DataGridExample from "../../components/DataGridExample"
+import { Link } from "react-router-dom"
 
 const DisplayUserTwo = () => {
   const [displayAlluser, setDisplayAllUser] = useState([])
@@ -11,21 +12,25 @@ const DisplayUserTwo = () => {
     displayUser()
   }, [])
 
-  // const rows = [
-  //     { id: 1, id: 'Hello', firstName: 'World' },
-  //     { id: 2, id: 'DataGridPro', firstName: 'is Awesome' },
-  //     { id: 3, id: 'MUI', firstName: 'is Amazing' },
-  //   ];
 
+  const handleEdit = (id) =>{
+    console.log("Selected row id is ", id);
+  }
+ 
   const columns = [
-    { field: "id", headerName: "ID", width: 150 },
+    { field: "id", headerName: "ID", width: 150 }, 
     { field: "firstName", headerName: "First", width: 150 },
-    { field: "lastName", headerName: "Last", width: 150 },
+    { field: "lastName", headerName: "Last", width: 150, renderCell: (params) =>{
+        return <Link to="/" >{params.row.lastName}</Link>
+    } },
     { field: "fullName", headerName: "Full Name", width: 150 },
     { field: "email", headerName: "Email", width: 150 },
     { field: "designation", headerName: "Designation", width: 150 },
     { field: "department", headerName: "Department", width: 150 },
-    // { field: "profile", headerName: "Profile", width: 150, },
+    { field: "Edit", headerName: "Edit", width: 150, renderCell: (params)=>{
+        return <button onClick={(id)=> handleEdit(params.row.id) }>id is  {params.row.id}</button> 
+    }},
+    
   ]
 
   const displayUser = async () => {
