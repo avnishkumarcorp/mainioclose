925fbfae8161d509bed09db5b11581e473bc6950
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-10-16 13:45:38 +0530

    add notes ui and integrate lead comment

---
+++ src/API/GetQuery.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+export const getQuery = (URL) =>{
+    return(
+        axios.get(URL, {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+              },
+        })
+    )
+}
