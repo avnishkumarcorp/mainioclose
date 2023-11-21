19c582d6083bfa462070ebb119d6998afc9b50a5
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-11-21 14:03:25 +0530

    add estimate tabs

--- src/Main/Sales/Leads/EstimateDesignPage.js
+++ src/Main/Sales/Leads/EstimateDesignPage.js
@@ -1,21 +1,31 @@
 import React from "react"
 import "./EstimateDesignPage.scss"
 
-const EstimateDesignPage = ({setEstimateOpenBtn}) => {
-
-    const closeEstimate = () => {
-        setEstimateOpenBtn((prev) => !(prev)); 
-    }
+const EstimateDesignPage = ({ setEstimateOpenBtn }) => {
+  const closeEstimate = () => {
+    setEstimateOpenBtn((prev) => !prev)
+  }
 
   return (
-  <div className="estimate-ui-design">
-    <div onClick={()=> closeEstimate()} className="estimate-close">
+    <div className="estimate-ui-design">
+      <div onClick={() => closeEstimate()} className="estimate-close">
         {/* <p >close</p> */}
-    <div className="cross-two-icon"><i class="fa-solid fa-xmark"></i></div>
+        <div className="cross-two-icon">
+          <i class="fa-solid fa-xmark"></i>
+        </div>
+      </div>
+      <div className="estimate-header">
+        <div>
+          <h2 className="estimate-text"><i className="fa-solid fa-calendar-days mr-2"></i>Estimate Details</h2>
+
+        </div>
+        <div>
+          <button className="estimate-tabs">Notes</button>
+          <button className="estimate-tabs">Edit</button>
+          <button className="estimate-tabs">Convert to Invoice</button>
+        </div>
+      </div>
     </div>
-    <h1>heelo</h1>
-    <h2>i am Estimate</h2>
-  </div>
   )
 }
 
