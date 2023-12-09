a964a0c5af80c7c90bb2098057cdf37c7fae94b7
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-12-09 12:01:31 +0530

    add mui table filter for admin"

--- src/Tables/UserLeadComponent.js
+++ src/Tables/UserLeadComponent.js
@@ -1,17 +1,29 @@
 import { DataGrid, GridToolbar } from "@mui/x-data-grid"
 import React from "react"
+import { useSelector } from "react-redux"
 
-const UserLeadComponent = ({row, columns, tableName, getRowId}) => {
+const UserLeadComponent = ({ row, columns, tableName, getRowId }) => {
+  
+  const currentUserRoles = useSelector(
+    (prev) => prev.AuthReducer.currentUser.roles
+  )
+  const adminRole = currentUserRoles.includes("ADMIN")
+  
   return (
     <div>
       <h1 className="table-main-heading">{tableName}</h1>
-      <DataGrid checkboxSelection rows={row} getRowId={getRowId} columns={columns} slots={{ toolbar: GridToolbar }}
+      <DataGrid
+        checkboxSelection
+        rows={row}
+        getRowId={getRowId}
+        columns={columns}
+        slots={ adminRole ? { toolbar: GridToolbar } : ""}
         slotProps={{
           toolbar: {
             showQuickFilter: true,
           },
-        }}
- />
+        }}  
+      />
     </div>
   )
 }
