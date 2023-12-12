1ff389527ec37dd814fb6344a48dddeaa8c83e41
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-12-12 15:30:55 +0530

    remove create lead variable

--- src/Model/LeadCreateModel.js
+++ src/Model/LeadCreateModel.js
@@ -14,7 +14,6 @@ const LeadCreateModel = () => {
     leadDescription: "",
     mobileNo: "",
     urls: "",
-    createDate: "",
     lastUpdated: "",
     latestStatusChangeDate: "",
     source: "",
