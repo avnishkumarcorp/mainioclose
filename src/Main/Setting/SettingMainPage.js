d4a4d48cb2823ab9224b6fdcba5c5d500ba5f284
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-12-13 12:04:31 +0530

    add setting page

--- src/Main/Setting/SettingMainPage.js
+++ src/Main/Setting/SettingMainPage.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const SettingMainPage = () => {
-  return <div>SettingMainPage</div>;
+  return (
+    <div className="setting">
+      <div className="side-links">
+        <p>Side Links</p>
+      </div>
+      <div className="side-data">
+      <p>Side Data</p>
+    
+      </div>
+    </div>
+  )
 };
 
 export default SettingMainPage;
