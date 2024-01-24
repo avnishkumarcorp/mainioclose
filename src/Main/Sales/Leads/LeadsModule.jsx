fd81ac1da13435cf59f5dc881e2c0bae83f947ce
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-01-24 15:39:36 +0530

    add multiassignee

--- src/Main/Sales/Leads/LeadsModule.jsx
+++ src/Main/Sales/Leads/LeadsModule.jsx
@@ -18,6 +18,8 @@ import TableScalaton from "../../../components/TableScalaton"
 import { getQuery } from "../../../API/GetQuery"
 import { putQuery } from "../../../API/PutQuery"
 import ArrowComponent from "../../../components/ArrowComponent"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import Checkbox from "@mui/material/Checkbox"
 
 const LeadsModule = () => {
   const [activeTab, setActiveTab] = useState(false)
@@ -28,12 +30,50 @@ const LeadsModule = () => {
   const [getAllStatus, setGetAllStatus] = useState([])
   const [statusDataId, setStatusDataId] = useState([])
   const [leadStatusD, setLeadStatusD] = useState(false)
-  const [toDate, setToDate] = useState("");
-  const [fromDate, setFromDate] = useState("");
-  const [dateFilter, setDateFilter] = useState(false);
+  const [toDate, setToDate] = useState("")
+  const [fromDate, setFromDate] = useState("")
+  const [dateFilter, setDateFilter] = useState(false)
 
-  console.log("data data", toDate);
-  console.log("from date", fromDate);
+  const location = useLocation()
+  const currentPath = location.pathname.split()
+  const splitPath = currentPath[0].split("/")
+  const currentUserId = Number(splitPath[2])
+  const currentLeadId = Number(splitPath[4])
+
+  const [selectedRows, setSelectedRows] = useState([])
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      setSelectedRows(allLeadData.map((row) => row.id))
+    } else {
+      setSelectedRows([])
+    }
+  }
+
+  const handleCheckboxClick = (event, id) => {
+    if (event.target.checked) {
+      setSelectedRows((prevSelected) => [...prevSelected, id])
+    } else {
+      setSelectedRows((prevSelected) =>
+        prevSelected.filter((rowId) => rowId !== id)
+      )
+    }
+  }
+
+  const [multiLeadData, setMultiLeadData] = useState({
+    leadIds: selectedRows,
+    statusId: null,
+    assigneId: 0,
+    updatedById: currentUserId,
+  })
+
+  useEffect(() => {
+    setMultiLeadData((prev) => ({ ...prev, leadIds: selectedRows }))
+  }, [handleSelectAllClick])
+
+  console.log("i amdjdjkk", multiLeadData)
+
+  console.log("selected Rowscvcfvfv", selectedRows)
 
   useEffect(() => {
     getAllLead()
@@ -47,43 +87,64 @@ const LeadsModule = () => {
     getAllStatusData()
   }, [])
 
-  const location = useLocation()
-  const currentPath = location.pathname.split()
-  const splitPath = currentPath[0].split("/")
-  const currentUserId = Number(splitPath[2])
-  const currentLeadId = Number(splitPath[4])
-
   const viewHistory = async (leadId) => {
-    try{
-    const singlePage = await putQuery(`/leadService/api/v1/lead/viewHistoryCreate?userId=${currentUserId}&leadId=${leadId}`)
-    console.log(singlePage);
-    }catch(err){
-      console.log(err);
+    try {
+      const singlePage = await putQuery(
+        `/leadService/api/v1/lead/viewHistoryCreate?userId=${currentUserId}&leadId=${leadId}`
+      )
+      console.log(singlePage)
+    } catch (err) {
+      console.log(err)
     }
   }
-
 
   const currentUserRoles = useSelector(
     (prev) => prev.AuthReducer.currentUser.roles
   )
   const adminRole = currentUserRoles.includes("ADMIN")
-  const newRole = currentUserRoles.includes("NEW") 
+  const newRole = currentUserRoles.includes("NEW")
+
+  const getDataId = () => {
+    console.log("i am click")
+  }
 
   const columns = [
-    { field: "id", headerName: "S.No", width: 150, filterable: false, renderCell: (props) =>{
-      console.log(props.api.getColumnIndexRelativeToVisibleColumns(props.row.index));
-
-      return (
-        <p>{props.api.getRowIndexRelativeToVisibleRows(props.row.id) + 1}</p>
-      )
-    } },
+    {
+      field: "select",
+      headerName: "Select",
+      width: 100,
+      renderCell: (params) => (
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={selectedRows.includes(params.row.id)}
+              onChange={(event) => handleCheckboxClick(event, params.row.id)}
+            />
+          }
+        />
+      ),
+    },
+    {
+      field: "id",
+      headerName: "S.No",
+      width: 150,
+      filterable: false,
+      renderCell: (props) => {
+        return (
+          <p>{props.api.getRowIndexRelativeToVisibleRows(props.row.id) + 1}</p>
+        )
+      },
+    },
     {
       field: "leadName",
       headerName: "Name",
       width: 150,
       renderCell: (props) => {
         return (
-          <Link to={`/erp/${currentUserId}/sales/leads/${props.row.id}`} onClick={() => viewHistory(props.row.id) }>
+          <Link
+            to={`/erp/${currentUserId}/sales/leads/${props.row.id}`}
+            onClick={() => viewHistory(props.row.id)}
+          >
             {props?.row?.leadName}
           </Link>
         )
@@ -97,9 +158,20 @@ const LeadsModule = () => {
         return <p className="mb-0">{props?.row?.assignee?.fullName}</p>
       },
     },
-    { field: "createdBy", headerName: "Created By", width: 150, renderCell: (props) => {
-      return <p className="mb-0">{props?.row?.createdBy?.fullName ? props?.row?.createdBy?.fullName : "NA" }</p> 
-    }  },
+    {
+      field: "createdBy",
+      headerName: "Created By",
+      width: 150,
+      renderCell: (props) => {
+        return (
+          <p className="mb-0">
+            {props?.row?.createdBy?.fullName
+              ? props?.row?.createdBy?.fullName
+              : "NA"}
+          </p>
+        )
+      },
+    },
     { field: "mobileNo", headerName: "Mobile No", width: 150 },
     { field: "email", headerName: "Email", width: 150 },
     {
@@ -142,11 +214,15 @@ const LeadsModule = () => {
       width: 150,
       renderCell: (props) => {
         const leadStatus = props.row.status?.name
-        return <p className={leadStatus === "New" ? "lead-new": ""}>{leadStatus ? leadStatus : "NA"}</p>
+        return (
+          <p className={leadStatus === "New" ? "lead-new" : ""}>
+            {leadStatus ? leadStatus : "NA"}
+          </p>
+        )
       },
     },
     { field: "source", headerName: "Source", width: 150 },
-     {
+    {
       field: "action",
       headerName: "Action",
       width: 150,
@@ -186,7 +262,6 @@ const LeadsModule = () => {
   const changeLeadAssignee = async (id, leadId) => {
     try {
       const updatePerson = await axios.put(
-        
         `/leadService/api/v1/lead/updateAssignee?leadId=${leadId}&userId=${id}&updatedById=${currentUserId}`,
         {
           headers: {
@@ -219,11 +294,6 @@ const LeadsModule = () => {
   const getAllLead = async () => {
     try {
       const allLead = await axios.get(
-        // /leadService/api/v1/lead/getAllLead?userId=1&statusId=2
-        // /leadService/api/v1/lead/getAllLead?userId=1&statusId=1&toDate=23&fromDate=23
-        // console.log("data data", toDate);
-        // console.log("from date", fromDate);
-
         `/leadService/api/v1/lead/getAllLead?userId=${currentUserId}&statusId=${statusDataId}&toDate=${toDate}&fromDate=${fromDate}`,
         {
           headers: {
@@ -232,7 +302,6 @@ const LeadsModule = () => {
           },
         }
       )
-      console.log("all lead", allLead);
       const leadData = allLead.data.reverse()
       setAllLeadData(leadData)
       setLeadScalatonCall(false)
@@ -254,9 +323,21 @@ const LeadsModule = () => {
       }
     }
   }
+
+  const multiAssignee = async () => {
+    try {
+      const multiAssigneeCol = await putQuery(
+        "/leadService/api/v1/lead/updateMultiLeadAssigne",
+        multiLeadData
+      )
+      console.log("multidata", multiAssigneeCol)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   // MuiDataGrid-main css-204u17-MuiDataGrid-main
 
- 
   return (
     <div className="lead-module small-box-padding">
       <div className="create-user-box">
@@ -264,44 +345,138 @@ const LeadsModule = () => {
         {adminRole ? <LeadCreateModel /> : ""}
       </div>
 
-    <div className="all-between">
-      <div className="one-line">
-      <p className="my-2">
-        <select
-          className="p-1 status-select"
-          name="status"
-          onChange={(e) => setStatusDataId(e.target.value)}
-          id="status"
-          form="statusChange"
-        >
-          <option>Filter Status</option>
-          {getAllStatus.map((status, index) => (
-            <option value={status.id} key={index}>
-              {status.name}
-            </option>
-          ))}
-        </select>
-      </p>
+      <div className="all-between">
+        <div className="one-line">
+          <p className="my-2">
+            <select
+              className="p-1 status-select"
+              name="status"
+              onChange={(e) => setStatusDataId(e.target.value)}
+              id="status"
+              form="statusChange"
+            >
+              <option>Filter Status</option>
+              {getAllStatus.map((status, index) => (
+                <option value={status.id} key={index}>
+                  {status.name}
+                </option>
+              ))}
+            </select>
+          </p>
 
-      <button className="common-btn-one" onClick={()=> window.location.reload()}>Remove Filter</button>
+          <button
+            className="common-btn-one"
+            onClick={() => window.location.reload()}
+          >
+            Remove Filter
+          </button>
+        </div>
+        <div>
+          <input
+            className="mr-2"
+            onChange={(e) => setToDate(e.target.value)}
+            type="date"
+          />
+          <input
+            className="mr-2"
+            onChange={(e) => setFromDate(e.target.value)}
+            type="date"
+          />
+          <button
+            className="common-btn-one"
+            onClick={() => setDateFilter((prev) => !prev)}
+          >
+            Apply
+          </button>
+        </div>
       </div>
-      <div>
-        <input className="mr-2"  onChange={(e)=> setToDate(e.target.value)} type="date" />
-        <input className="mr-2" onChange={(e)=> setFromDate(e.target.value)} type="date" />
-        <button className="common-btn-one" onClick={()=> setDateFilter((prev) => !(prev))}>Apply</button>
-   
-      </div>
-      </div>
-      
+
       <div className="table-arrow">
-         <ArrowComponent />
-      {leadScalatonCall ? (
-        <TableScalaton />
-      ) : (
-        <UserLeadComponent tableName={""} columns={columns} row={allLeadData} getRowId= {(row) => row.id} />
-      )}
+        {/* <ArrowComponent /> */}
+        {leadScalatonCall ? (
+          <TableScalaton />
+        ) : (
+          <UserLeadComponent
+            tableName={""}
+            columns={columns}
+            row={allLeadData}
+            getRowId={(row) => row.id}
+            onSelectionModelChange={(newSelection) =>
+              setSelectedRows(newSelection)
+            }
+            selectionModel={selectedRows}
+            components={{
+              Toolbar: () => (
+                <>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        indeterminate={
+                          selectedRows.length > 0 &&
+                          selectedRows.length < allLeadData.length
+                        }
+                        checked={selectedRows.length === allLeadData.length}
+                        onChange={handleSelectAllClick}
+                      />
+                    }
+                    label="Select All"
+                  />
+                </>
+              ),
+            }}
+          />
+        )}
 
-  </div>
+        <div className="bottom-line">
+          <div>
+            <select
+              className="p-1 status-select"
+              name="status"
+              onChange={(e) =>
+                setMultiLeadData((prev) => ({
+                  ...prev,
+                  statusId: e.target.value,
+                }))
+              }
+              id="status"
+              form="statusChange"
+            >
+              <option>Select Status</option>
+              {getAllStatus.map((status, index) => (
+                <option value={status.id} key={index}>
+                  {status.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <select
+              className="p-1 status-select"
+              onChange={(e) =>
+                setMultiLeadData((prev) => ({
+                  ...prev,
+                  assigneId: e.target.value,
+                }))
+              }
+              // onSelect={(e)=> }
+              name="lead"
+              id="lead"
+            >
+              <option>Select User</option>
+              {leadUserNew.map((user, index) => (
+                <option key={index} value={user.id}>
+                  {user?.fullName}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <button onClick={() => multiAssignee()} className="common-btn-one">
+              Assign
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -5,39 +5,39 @@ import { Link } from "react-router-dom"
 const NotFoundPage = () => {
   return (
     <>
-      <div class="flex-container">
-        <div class="text-center">
+      <div className="flex-container">
+        <div className="text-center">
           <h1>
-            <span class="fade-in" id="digit1">
+            <span className="fade-in" id="digit1">
               4
             </span>
-            <span class="fade-in" id="digit2">
+            <span className="fade-in" id="digit2">
               0
             </span>
-            <span class="fade-in" id="digit3">
+            <span className="fade-in" id="digit3">
               4
             </span>
           </h1>
-          <h3 class="fadeIn">PAGE NOT FOUND</h3>
+          <h3 className="fadeIn">PAGE NOT FOUND</h3>
           <Link to='/' className="found-btn">
             Return To Home
           </Link>
         </div>
       </div>
-      <div class="flex-container">
-        <div class="text-center">
+      <div className="flex-container">
+        <div className="text-center">
           <h1>
-            <span class="fade-in" id="digit1">
+            <span className="fade-in" id="digit1">
               4
             </span>
-            <span class="fade-in" id="digit2">
+            <span className="fade-in" id="digit2">
               0
             </span>
-            <span class="fade-in" id="digit3">
+            <span className="fade-in" id="digit3">
               4
             </span>
           </h1>
-          <h3 class="fadeIn">PAGE NOT FOUND</h3> 
+          <h3 className="fadeIn">PAGE NOT FOUND</h3> 
         </div>
       </div>
     </>
