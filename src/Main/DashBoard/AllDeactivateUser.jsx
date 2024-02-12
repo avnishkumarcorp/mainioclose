fc3bf368256f450da198e79090cf1168e00e5abf
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-02-12 16:36:33 +0530

    add active user functionality

--- src/Main/DashBoard/AllDeactivateUser.jsx
+++ src/Main/DashBoard/AllDeactivateUser.jsx
@@ -1,10 +1,83 @@
-import React from "react"
+import React, { useState } from "react"
+import { useCustomRoute } from "../../Routes/GetCustomRoutes"
+import UserListComponent from "../../Tables/UserListComponent"
+import { putQueryNoData } from "../../API/PutQueryWithoutData"
 
 const AllDeactivateUser = () => {
+  const [deactiveDep, setDeactiveDep] = useState(false)
+
+  const allDeactivateUser = `/leadService/api/v1/users/getAllDeactivateUser`
+  const deactivateUserDep = [deactiveDep]
+
+  const { productData: allDeactivateUsers, loading: userLoading } =
+    useCustomRoute(allDeactivateUser, deactivateUserDep)
+
+  console.log("cfrf", allDeactivateUsers)
+
+  const activateUserFun = async (id) => {
+    if (window.confirm("Are you sure to Activate this User?") == true) {
+      try {
+        const activeLogin = await putQueryNoData(
+          `/securityService/api/auth/activateUser?userId=${id}`
+        )
+        const activeLead = await putQueryNoData(
+          `/leadService/api/v1/users/activateUser?id=${id}`
+        )
+
+        console.log(activeLogin)
+        console.log(activeLead)
+        setDeactiveDep((prev) => !prev)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+
+  //   /securityService/api/auth/activateUser?userId=11
+  //   /leadService/api/v1/users/activateUser?id=11
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: 150,
+      renderCell: (props) => {
+        return <p className="mb-0">CORP00{props.row.id}</p>
+      },
+    },
+    { field: "fullName", headerName: "Full Name", width: 150 },
+    { field: "email", headerName: "Email", width: 240, hideable: false },
+    { field: "designation", headerName: "Designation", width: 150 },
+    { field: "department", headerName: "Department", width: 150 },
+    { field: "role", headerName: "Role", width: 150 },
+    {
+      field: "Action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (props) => {
+        return (
+          <button
+            className="btn btn-success"
+            onClick={() => activateUserFun(props.row.id)}
+          >
+            Activate
+          </button>
+        )
+      },
+    },
+  ]
+
   return (
     <div className="small-box-padding">
       <div className="create-user-box">
         <h1 className="table-heading">Deactivate Users</h1>
+      </div>
+      <div className="mt-3">
+        <UserListComponent
+          tableName={""}
+          columns={columns}
+          row={allDeactivateUsers}
+        />
       </div>
     </div>
   )
