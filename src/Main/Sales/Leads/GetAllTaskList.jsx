9e9e0a5954e700cc5022f5ecc52ab0be68702446
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-02-28 12:39:24 +0530

    add alll task with filter

--- src/Main/Sales/Leads/GetAllTaskList.jsx
+++ src/Main/Sales/Leads/GetAllTaskList.jsx
@@ -1,83 +1,115 @@
 import React from "react"
 import { useCustomRoute } from "../../../Routes/GetCustomRoutes"
 import { useParams } from "react-router-dom"
-import UserLeadComponent from "../../../Tables/UserLeadComponent";
+import UserLeadComponent from "../../../Tables/UserLeadComponent"
 
 const GetAllTaskList = () => {
-    const {userid} = useParams();
+  const { userid } = useParams()
 
-    const allTasksByUser = `/leadService/api/v1/task/getAllTaskByAssignee?assigneeId=${userid}`
-    const allTaskDep = []
-  
-    const {
-      productData: taskData,
-      loading: taskLoading,
-      error,
-    } = useCustomRoute(allTasksByUser, allTaskDep)
+  const allTasksByUser = `/leadService/api/v1/task/getAllTaskByAssignee?assigneeId=${userid}`
+  const allTaskDep = []
 
+  const {
+    productData: taskData,
+    setProductData,
+    loading: taskLoading,
+    error,
+  } = useCustomRoute(allTasksByUser, allTaskDep)
 
-    console.log("task data ", taskData);
+  console.log("task data ", taskData)
 
-    const columns = [
-      {
-        field: "id",
-        headerName: "S.No",
-        width: 60,
-        filterable: false,
-        renderCell: (props) => {
-          return (
-            <p className="mb-0">
-              {props.api.getRowIndexRelativeToVisibleRows(props.row.id) + 1}
-            </p>
-          )
-        },
+  let beforeDate = Date.now()
+  let endDate = Date.now() + 86400000
+
+  const todayTaskData = () => {
+    let taskData2 = taskData
+
+    // const data = taskData.filter((task) => { return new Date(task.expectedDate).getTime()})
+    const data = taskData.filter((task) => {
+      let taskD = new Date(task.expectedDate).getTime()
+      return taskD >= beforeDate
+    })
+    setProductData(data)
+  }
+
+  // const todayTaskData = () => {
+  //   let taskData2 = taskData
+
+  //   // const data = taskData.filter((task) => { return new Date(task.expectedDate).getTime()})
+  //   const data = taskData2.filter((task) => {
+  //     let beforeDate = Date.now()
+  //     let endDate = Date.now() + 86400000
+  //     let taskD = new Date(task.expectedDate).getTime()
+
+  //     if (taskD >= beforeDate && taskD <= endDate) {
+  //       return taskData2
+  //     }
+
+  //   })
+  // }
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "S.No",
+      width: 60,
+      filterable: false,
+      renderCell: (props) => {
+        return (
+          <p className="mb-0">
+            {props.api.getRowIndexRelativeToVisibleRows(props.row.id) + 1}
+          </p>
+        )
       },
-      {
-        field: "name",
-        headerName: "Name",
-        width: 200,  
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      width: 200,
+    },
+    {
+      field: "description",
+      headerName: "Description",
+      width: 500,
+    },
+    {
+      field: "expectedDate",
+      headerName: "Date",
+      width: 200,
+      renderCell: (props) => {
+        const data = props?.row?.expectedDate
+        // console.log(data)
+        return data === null || undefined ? (
+          "NA"
+        ) : (
+          <p>
+            {new Date(props.row.expectedDate).toLocaleDateString()} -{" "}
+            {new Date(props.row.expectedDate).getHours()}:
+            {new Date(props.row.expectedDate).getMinutes()}
+          </p>
+        )
       },
-      {
-        field: "description",
-        headerName: "Description",
-        width: 500,  
-      },
-      {
-        field: "expectedDate",
-        headerName: "Date",
-        width: 200,
-        renderCell: (props) => {
-          const data = props?.row?.expectedDate
-          // console.log(data)
-          return data === null || undefined ? (
-            "NA"
-          ) : (
-            <p>
-              {new Date(props.row.expectedDate).toLocaleDateString()} -{" "}
-              {new Date(props.row.expectedDate).getHours()}:
-              {new Date(props.row.expectedDate).getMinutes()}
-            </p>
-          )
-        },
-      }
-    ]
+    },
+  ]
 
-
-
-
-
-
-return (
-
-
-
-  <div className="lead-module small-box-padding">
-    <h1 className="table-heading">All Tasks</h1>
-    <div className="mt-3">
-      <UserLeadComponent row={taskData} columns={columns} />
+  return (
+    <div className="lead-module small-box-padding">
+      <div className="create-user-box">
+      <h1 className="table-heading">All Tasks</h1>
+      <div>
+      <button className="common-btn-one" onClick={todayTaskData}>
+        today task
+      </button>
+      <button className="common-btn-one ml-2" onClick={() => {window.location.reload()}}>
+        Remove filter
+      </button>
+      </div>
+      </div>
+      <div className="mt-3">
+        <UserLeadComponent row={taskData} columns={columns} />
+      </div>
     </div>
-  </div>
-)
+  )
 }
 
 export default GetAllTaskList
